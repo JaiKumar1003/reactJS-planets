beeb57c87566379fc26db52d1044d026656808fe
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 17:07:45 +0530

    Test Case Failed

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -6,7 +6,7 @@ const PlanetItem = props => {
   const {planetItem} = props
   const {imageUrl, name, description} = planetItem
   return (
-    <div className="planet-item" testid="planets">
+    <div className="planet-item" data-testid="planets">
       <img className="planet-img" src={imageUrl} alt={`planets ${name}`} />
       <h1 className="planet-heading">{name}</h1>
       <p className="planet-description">{description}</p>
