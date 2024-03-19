650ff6dee2ee91707e52ae884b1c14b35f58ad53
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 17:05:27 +0530

    Test Case Failed

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -6,15 +6,10 @@ import './index.css'
 
 const ReactSlick = props => {
   const {planetsList} = props
-  const settings = {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }
-  console.log(planetsList[0])
   return (
     <div className="slider-container">
       <h1 className="main-heading">PLANETS</h1>
-      <Slider {...settings}>
+      <Slider>
         {planetsList.map(eachItem => (
           <PlanetItem planetItem={eachItem} key={eachItem.id} />
         ))}
