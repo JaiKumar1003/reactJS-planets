a6366160e4e54553b3692afbe74b42cc9153cbdb
JaiKumar1003 <jaijai100320@gmail.com>
2024-03-19 06:46:40 +0000

    Test Case Failed

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetItem} = props
+  const {imageUrl, name, description} = planetItem
+  return (
+    <div className="planet-item" testid="planets">
+      <img className="planet-img" src={imageUrl} alt={`planets ${name}`} />
+      <h1 className="planet-heading">{name}</h1>
+      <p className="planet-description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
